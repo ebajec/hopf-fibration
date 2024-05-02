8a54a25562ae91e309df60afcb632fd8fd5a93d0
Elijah Bajec Dickinson <elijah.b@queensu.ca>
2024-05-02 19:51:44 -0400

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,29 @@
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
 
-#define WIDTH 800
-#define HEIGHT 800
-#define PI 3.141592654f
-#define SPH_ATTR_COUNT 3
-#define PATH_SIZE 100
-#define CIRC_SIZE 1000
+#define WIDTH 1280                  
+#define HEIGHT 720
 
+#define PI 3.141592654f
+
+#define SPH_ATTR_COUNT 3            
+
+#define CONTROL_VIEWPORT_SIZE 300   // Determines height/width of lower-right viewport
+
+#define FIBER_COUNT 100             // Number of fibers to compute
+#define FIBER_SIZE 1000             // Number of samples in each fiber
+
+#define HF_CAMERA_BINDING 0
+#define C_CAMERA_BINDING 1
+
+/**********************************************************************************
+ * 
+ * Helper functions
+ * 
+ ***********************************************************************************/
 vec3 S2(GLfloat phi, GLfloat theta) {
     return vec3{sin(theta)*cos(phi),sin(theta)*sin(phi),cos(theta)};
 }
-
 void printData(GLfloat* data, int x, int y) 
 {
     for (int i = 0; i < x; i++) {
@@ -28,8 +40,7 @@ void printData(GLfloat* data, int x, int y)
         std::cout << "\n";
     }
 }
-template<typename T> 
-std::vector<T> lerp(T start, T end, int steps) {
+template<typename T> std::vector<T> lerp(T start, T end, int steps) {
     std::vector<T>  out(steps+1);   
     T diff = end - start;
     float step = pow(steps,-1);
@@ -42,15 +53,16 @@ std::vector<T> lerp(T start, T end, int steps) {
 
 /**********************************************************************************
  *
- * Sphere.
+ * Sphere
  * 
  **********************************************************************************/
 template<int phi_step, int theta_step> class Sphere {
 public:
     Sphere() {}
+    ~Sphere();
     void init(GLuint usage = GL_STATIC_DRAW);
-    //void setScale(GLfloat s);
-    //void setPos(vec3 pos);
+    void setScale(GLfloat s);
+    void setPos(vec3 pos);
     void render(ShaderProgram shader);
     
     enum ATTR {POSITION,COLOR,NORMAL};
@@ -61,6 +73,15 @@ private:
     GLint draw_firsts[phi_step];        // For glMultiDrawArrays
     GLsizei draw_counts[theta_step];    // For glMultiDrawArrays
 };
+/**********************************************************************************
+ * 
+ * Implementation details for Sphere.
+ * 
+************************************************************************************/
+template<int phi_step, int theta_step> Sphere<phi_step,theta_step>::~Sphere() {
+    glDeleteBuffers(3,vbo);
+    glDeleteVertexArrays(1,&vao);
+}
 template<int phi_step, int theta_step> void Sphere<phi_step,theta_step>::init(GLuint usage) {
     int data_size = 8*phi_step*theta_step;
     GLfloat* data_pos = new GLfloat[data_size];
@@ -143,6 +164,10 @@ template<int phi_step, int theta_step> void Sphere<phi_step,theta_step>::render(
  * Main window/program.  Renders hopf fibration and includes UI to control
  * some parameters.
  * 
+ * Some things to note:
+ * - the "hf's" stands for "hopf fibration"
+ * - the "c's" stand for "control"
+  * 
  **********************************************************************************/
 class HopfSimulation : public BaseViewWindow {
 public:
@@ -151,20 +176,25 @@ public:
 
 protected:
     void _main();
+    bool _initFiberData();
+    bool _initShaders();
     void _renderFibers();
-    void _initFiberData();
     void _renderUI();
+
+    ShaderProgram c_shader;
+    Camera c_cam;
+    Sphere<30,30> c_sphere;
     
-    ShaderProgram hf_main_shader;               // Handles rendering of main scene 
-    ShaderProgram hf_map;                       // Computes fibers of hopf map
-    GLuint hf_vao;                              
-    GLuint hf_vbo_in;
-    GLuint hf_vbo_out;
-    GLfloat hf_data_in[4*PATH_SIZE*CIRC_SIZE];  // Arguments to hf_map; allows parallel computation of fibers.
-    GLint hf_draw_firsts[PATH_SIZE];            // For glMultiDrawArrays
-    GLsizei hf_draw_counts[PATH_SIZE];          // For glMultiDrawArrays
-    float hf_anim_speed;                        
-    int hf_draw_max = PATH_SIZE;                // Parameter to control number of drawn circles
+    ShaderProgram hf_main_shader;                   // Handles rendering of main scene 
+    ShaderProgram hf_map;                           // Computes fibers of hopf map
+    GLuint hf_vao;                               
+    GLuint hf_vbo_in; 
+    GLuint hf_vbo_out; 
+    GLfloat hf_data_in[4*FIBER_COUNT*FIBER_SIZE];   // Arguments to hf_map; allows parallel computation of fibers.
+    GLint hf_draw_firsts[FIBER_COUNT];              // For glMultiDrawArrays
+    GLsizei hf_draw_counts[FIBER_COUNT];            // For glMultiDrawArrays
+    float hf_anim_speed;                         
+    int hf_draw_max = FIBER_COUNT;                  // Parameter to control number of drawn circles
 };
 
 /**********************************************************************************
@@ -172,11 +202,66 @@ protected:
  * Implementation details for HopfSimulation.
  * 
  **********************************************************************************/
-HopfSimulation::HopfSimulation(int width, int height) : BaseViewWindow(width, height) {}
+HopfSimulation::HopfSimulation(int width, int height) : BaseViewWindow(width, height) {
+    c_cam = Camera(vec3{1,0,0},vec3{-5,0,0},CONTROL_VIEWPORT_SIZE,CONTROL_VIEWPORT_SIZE,PI/3);
+}
 HopfSimulation::~HopfSimulation() {
     glDeleteBuffers(1,&hf_vbo_in);
     glDeleteBuffers(1,&hf_vbo_out);
     glDeleteVertexArrays(1,&hf_vao);
+}
+bool HopfSimulation::_initShaders() {
+
+    // Initialize main shader and camera
+
+    hf_main_shader.addShader("../shader/main_vertex.glsl",GL_VERTEX_SHADER);
+    hf_main_shader.addShader("../shader/main_frag.glsl",GL_FRAGMENT_SHADER);
+    hf_main_shader.link();
+
+    if (!_cam.bindToShader(hf_main_shader,"Camera",HF_CAMERA_BINDING)) return false;
+    
+    // Initialize hopf map shader and fiber data
+
+    hf_map.addShader("../shader/hopf_map.glsl",GL_VERTEX_SHADER);
+    const GLchar* hf_feedback_varyings[] = { "data_out" };
+    glTransformFeedbackVaryings(hf_map.program, 1, hf_feedback_varyings, GL_INTERLEAVED_ATTRIBS);
+    hf_map.link();
+
+    // Initialize control window shader and camera
+
+    c_shader.addShader("../shader/control_window_vertex.glsl",GL_VERTEX_SHADER);
+    c_shader.addShader("../shader/control_window_frag.glsl",GL_FRAGMENT_SHADER);
+    c_Shader.link();
+
+    if (!c_cam.bindToShader(c_shader,"Camera",C_CAMERA_BINDING)) return false;
+
+    return true;
+}   
+bool HopfSimulation::_initFiberData() 
+{
+    // Set up linearly spaced inputs for fiber calculation.
+    for (int i = 0; i < FIBER_COUNT; i++) {
+        hf_draw_firsts[i] = i*FIBER_SIZE;
+        hf_draw_counts[i] = FIBER_SIZE;
+
+        for (int j = 0; j < FIBER_SIZE; j++) {
+            hf_data_in[4*(FIBER_SIZE*i + j)] = (float)i/(float)FIBER_COUNT;
+            hf_data_in[4*(FIBER_SIZE*i + j) + 3] = (float)j/(float)FIBER_SIZE;
+        }     
+    }
+    // Prepare buffers for computing fibers of a hopf map
+    glGenVertexArrays(1, &hf_vao);
+    glBindVertexArray(hf_vao);
+    glEnableVertexAttribArray(0);
+    
+    glGenBuffers(1, &hf_vbo_in);
+    glBindBuffer(GL_ARRAY_BUFFER, hf_vbo_in);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(hf_data_in), hf_data_in, GL_STREAM_DRAW);
+
+    glGenBuffers(1, &hf_vbo_out);
+    glBindBuffer(GL_ARRAY_BUFFER, hf_vbo_out);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(hf_data_in), nullptr, GL_STATIC_READ);  
+    return true; 
 }
 void HopfSimulation::_renderFibers() 
 {
@@ -195,7 +280,7 @@ void HopfSimulation::_renderFibers()
 
     glBeginTransformFeedback(GL_POINTS);
 
-    glDrawArrays(GL_POINTS, 0, PATH_SIZE*CIRC_SIZE);
+    glDrawArrays(GL_POINTS, 0, FIBER_COUNT*FIBER_SIZE);
 
     glEndTransformFeedback();
 
@@ -211,31 +296,6 @@ void HopfSimulation::_renderFibers()
     glMultiDrawArrays(GL_LINE_LOOP,hf_draw_firsts,hf_draw_counts,hf_draw_max);
 
 }
-void HopfSimulation::_initFiberData() 
-{
-    // Set up linearly spaced inputs for fiber calculation.
-    for (int i = 0; i < PATH_SIZE; i++) {
-        hf_draw_firsts[i] = i*CIRC_SIZE;
-        hf_draw_counts[i] = CIRC_SIZE;
-
-        for (int j = 0; j < CIRC_SIZE; j++) {
-            hf_data_in[4*(CIRC_SIZE*i + j)] = (float)i/(float)PATH_SIZE;
-            hf_data_in[4*(CIRC_SIZE*i + j) + 3] = (float)j/(float)CIRC_SIZE;
-        }     
-    }
-    // Prepare buffers for computing fibers of a hopf map
-    glGenVertexArrays(1, &hf_vao);
-    glBindVertexArray(hf_vao);
-    glEnableVertexAttribArray(0);
-    
-    glGenBuffers(1, &hf_vbo_in);
-    glBindBuffer(GL_ARRAY_BUFFER, hf_vbo_in);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(hf_data_in), hf_data_in, GL_STREAM_DRAW);
-
-    glGenBuffers(1, &hf_vbo_out);
-    glBindBuffer(GL_ARRAY_BUFFER, hf_vbo_out);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(hf_data_in), nullptr, GL_STATIC_READ);   
-}
 void HopfSimulation::_renderUI()
 {
     // Start the Dear ImGui frame
@@ -245,7 +305,7 @@ void HopfSimulation::_renderUI()
 	// Parameters controls
 	ImGui::Begin("Parameters");                          
 	ImGui::SliderFloat("Animation speed", &this->hf_anim_speed, 0.0f, 1.0f);
-	ImGui::SliderInt("Circle count", &this->hf_draw_max, 0, PATH_SIZE);
+	ImGui::SliderInt("Circle count", &this->hf_draw_max, 0, FIBER_COUNT);
 	ImGui::End();
 	// Rendering
 	ImGui::Render();
@@ -262,42 +322,37 @@ void HopfSimulation::_main()
     ImGui_ImplGlfw_InitForOpenGL(_window, true);
     ImGui_ImplOpenGL3_Init("#version 330");
 
-    // Initialize main shader
-    hf_main_shader.addShader("../shader/vertex_main.glsl",GL_VERTEX_SHADER);
-    hf_main_shader.addShader("../shader/frag_main.glsl",GL_FRAGMENT_SHADER);
-    hf_main_shader.link();
-
-    if (!_cam.bindToShader(hf_main_shader,"Camera",0)) return;
-    
-
-    // Initialize hopf map shader and fiber data
-    hf_map.addShader("../shader/hopf_map.glsl",GL_VERTEX_SHADER);
-    const GLchar* feedbackVaryings[] = { "data_out" };
-    glTransformFeedbackVaryings(hf_map.program, 1, feedbackVaryings, GL_INTERLEAVED_ATTRIBS);
-    hf_map.link();
+    if (!_initShaders()) {
+        printf("Failed to initialize shaders.\n");
+        return;
+    }
     _initFiberData();
 
-    // Initialize control ball
-    Sphere<30,30> sphere;
-    sphere.init();
+    c_sphere.init();
 
     while (!glfwWindowShouldClose(_window)) {
         glfwPollEvents();
 
         // Correct window scaling
     	glfwGetFramebufferSize(_window, &_width, &_height);
-    	glViewport(0, 0, _width, _height);
 
         // Clear buffer
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        //Initialize camera
-		_cam.setScreenRatio(_width,_height);
+        _cam.setScreenRatio(_width,_height);
         _cam.updateUniformData();
-        
+
         //Rendering
-        sphere.render(hf_main_shader);
+
+        // Render sphere in bottom right corner
+        glViewport(_width - CONTROL_VIEWPORT_SIZE, 0, _width, CONTROL_VIEWPORT_SIZE);
+        c_sphere.render(hf_main_shader);
+
+        // Render hopf fibration in main viewport
+        glViewport(0, 0, _width, _height);
         _renderFibers();
+
+
         _renderUI();
     	glfwSwapBuffers(_window);    
     }
